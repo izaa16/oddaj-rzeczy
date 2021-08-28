251ccbbbd6ff782fd0e0f6b036f76ca61e22045d
izaa16 <72252280+izaa16@users.noreply.github.com>
2021-08-28 20:19:54 +0200

    home info

--- oddaj-rzeczy/src/components/Home/Home.js
+++ oddaj-rzeczy/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomeHeader from './HomeHeader';
 import HomeStart from './HomeStart';
+import HomeInfo from './HomeInfo';
 
 const Home = () => {
     return (
@@ -14,6 +15,7 @@ const Home = () => {
                     <HomeStart />
                 </div>
             </div>
+            <HomeInfo />
         </>
     )
 }
---
+++ oddaj-rzeczy/src/components/Home/HomeInfo.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const HomeInfo = () => {
+    return (
+        <div className='three-cols'>
+            <div className='col'>
+                <h1>10</h1>
+                <span>Oddanych worków</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className='col'>
+                <h1>5</h1>
+                <span>Wspartych organizacji</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className='col'>
+                <h1>7</h1>
+                <span>Zorganizowanych zbiórek</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+    )
+}
+export default HomeInfo;
