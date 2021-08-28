346123d2d53405383ccf6a62784f754d291c7a29
izaa16 <izaa16@o2.pl>
2021-08-28 13:59:33 +0200

    home header update

--- oddaj-rzeczy/src/components/Home/Home.js
+++ oddaj-rzeczy/src/components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import HomeHeader from './HomeHeader';
 
 const Home = () => {
     return (
-        <>
-
-        </>
+        <div className='container top-block flex' name="homeStart">
+            <div className='left-side-container' />
+            <div className='right-side-container'>
+                <div className='flex-col-right-top__header'>
+                    <HomeHeader />
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ oddaj-rzeczy/src/components/Home/HomeHeader.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const HomeHeader = () => {
+    return (
+        <>
+            <div className='header'>
+                <div className="buttons">
+                    <Link className='button-login' to="/logowanie"> Zaloguj się </Link>
+                    <Link className='button-register' to="/rejestracja"> Załóż konto </Link>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default HomeHeader;
